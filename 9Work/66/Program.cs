d4c0f55b6e0d553ca8aaec2e3db8832fd0b0fae3
ArtemP <artem93.01@mail.ru>
2023-01-08 16:36:58 +0300

    9 ДЗ

---
+++ 9Work/66/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void SumNumbers (int m, int n, int sum)
+{
+    if (m > n) 
+    {
+        Console.WriteLine(sum); 
+        return;
+    }
+    sum = sum + (m++);
+    SumNumbers(m, n, sum);
+}
+
+int EnterNumber(string message)
+{
+    System.Console.Write(message);
+    int number = int.Parse(System.Console.ReadLine());
+    return number;
+}
+
+int m = EnterNumber ("Введите число M : ");
+int n = EnterNumber ("Введите число N : ");
+SumNumbers(m, n, 0);
---
+++ 9Work/68/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Akker(int m, int n)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Akker(n - 1, 1);
+    else
+        return Akker(n - 1, Akker(n, m - 1));
+}
+int EnterNumber(string message)
+{
+    System.Console.Write(message);
+    int number = uint.Parse(System.Console.ReadLine());
+    return number;
+}
+
+int m = EnterNumber ("Введите число M : ");
+int n = EnterNumber ("Введите число N : ");
+int result = Akker(m, n);
+System.Console.WriteLine(result);
